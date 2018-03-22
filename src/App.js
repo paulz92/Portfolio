ab80cb6988320eaffcc9c4f57459ef690ddc78ab
Paul Zavattieri <paulz92@gmail.com>
2018-03-22 01:50:21 -0400

    master

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <p>This will be my portfolio soon.</p>
+        <p>Portfolio coming soon.</p>
       </div>
     );
   }
