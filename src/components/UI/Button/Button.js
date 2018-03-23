685e9084be9303ef53c659bca7faee88cdd6f559
Paul Zavattieri <paulz92@gmail.com>
2018-03-23 15:02:06 -0400

    button transition/transform mui@next

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import classes from './Button.css';
+import { KeyboardArrowRight } from 'material-ui-icons'
 
 const button = (props) => (
   <button
     disabled={props.disabled}
     className={[classes.Button, classes[props.btnType]].join(' ')}
-    onClick={props.clicked}>{props.children}</button>
+    onClick={props.clicked}>
+      <p className={classes.buttonP}>More about me</p> 
+      <KeyboardArrowRight className={classes.arrowIcon} />  
+  </button>
 );
 
 export default button;
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import classes from './Home.css'
 import { Link } from 'react-router-dom'
 import Button from '../../components/UI/Button/Button'
-import AccessAlarmIcon from 'material-ui-icons/AccessAlarm'
 
 class Home extends Component {
   render() {
@@ -12,8 +11,8 @@ class Home extends Component {
           <p>Hey, I'm <span className={classes.name}>Paul Zavattieri</span>.</p>
           <p>I'm a front end web developer.</p>
         </div>
-        <Link to="/about">
-          <Button>More about me<AccessAlarmIcon /></Button>
+        <Link to="/about" className={classes.link}>
+          <Button />
         </Link>
       </div>
     )
