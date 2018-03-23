c9e7909273f1490052fd1887244f03144aeec764
Paul Zavattieri <paulz92@gmail.com>
2018-03-23 14:05:06 -0400

    home page

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
+import classes from './Home.css'
+import { Link } from 'react-router-dom'
+import Button from '../../components/UI/Button/Button'
+import AccessAlarmIcon from 'material-ui-icons/AccessAlarm'
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <p>Home</p>
+      <div className={classes.homeWrap}>
+        <div className={classes.contentWrap}>
+          <p>Hey, I'm <span className={classes.name}>Paul Zavattieri</span>.</p>
+          <p>I'm a front end web developer.</p>
+        </div>
+        <Link to="/about">
+          <Button>More about me<AccessAlarmIcon /></Button>
+        </Link>
       </div>
     )
   }
