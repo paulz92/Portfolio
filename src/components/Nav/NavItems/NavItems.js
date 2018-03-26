422a84cd43264d4915b94722bd8a767dbb75f821
Paul Zavattieri <paulz92@gmail.com>
2018-03-26 02:36:43 -0400

    update nav

--- src/components/Nav/NavItems/NavItems.js
+++ src/components/Nav/NavItems/NavItems.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import classes from './NavItems.css'
 import NavItem from './NavItem/NavItem'
+import Logo from '../../Logos/PZ/PZ'
 
 const navItems = (props) => {
   return (
     <ul className={classes.NavItems}>
-      <NavItem link="/" exact>HOME</NavItem>
-      <NavItem link="/about">ABOUT</NavItem>
-      <NavItem link="/skills">SKILLS</NavItem>
-      <NavItem link="/projects">PROJECTS</NavItem>
-      <NavItem link="/contact">CONTACT</NavItem>
+      <div className={classes.leftLinks} >
+        <NavItem link="/about">ABOUT</NavItem>
+        <NavItem link="/skills">SKILLS</NavItem>
+      </div>
+      <div className={classes.logoRoot}>
+        <Logo goBack={props.goBack} goForward={props.goForward} />
+      </div>
+      <div className={classes.rightLinks} >
+        <NavItem link="/projects">PROJECTS</NavItem>
+        <NavItem link="/contact">CONTACT</NavItem>
+      </div>
     </ul>
   )
 }
